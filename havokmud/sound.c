d00b5b6f336e5f1da3b5d5791ee07fd74f6b7e17
gjhurlbu <gjhurlbu@69fa473d-82f3-0310-a870-f1ebe01cfa2b>
2004-03-03 06:28:30 +0000

    - formatted with 'indent -orig -sob -npsl', cleanup
    git-svn-id: https://trac.beirdo.ca:4443/svn/havokmud/trunk@757 69fa473d-82f3-0310-a870-f1ebe01cfa2b

--- havokmud/sound.c
+++ havokmud/sound.c
@@ -1,126 +1,143 @@
 /*
-  DaleMUD v2.0	Released 2/1994
-  See license.doc for distribution terms.   DaleMUD is based on DIKUMUD
-*/
+ * DaleMUD v2.0 Released 2/1994 See license.doc for distribution terms.
+ * DaleMUD is based on DIKUMUD 
+ */
 
 #include <stdio.h>
 #include <string.h>
 
 #include "protos.h"
 
-/* extern variables */
+/*
+ * extern variables 
+ */
 
 extern struct obj_data *object_list;
 extern struct char_data *character_list;
 
 int RecGetObjRoom(struct obj_data *obj)
 {
-  if (obj->in_room != NOWHERE) {
-    return(obj->in_room);
-  }
-  if (obj->carried_by) {
-    return(obj->carried_by->in_room);
-  }
-  if (obj->equipped_by) {
-    return(obj->equipped_by->in_room);
-  }
-  if (obj->in_obj) {
-    return(RecGetObjRoom(obj->in_obj));
-  }
+    if (obj->in_room != NOWHERE) {
+        return (obj->in_room);
+    }
+    if (obj->carried_by) {
+        return (obj->carried_by->in_room);
+    }
+    if (obj->equipped_by) {
+        return (obj->equipped_by->in_room);
+    }
+    if (obj->in_obj) {
+        return (RecGetObjRoom(obj->in_obj));
+    }
 }
 
 void MakeNoise(int room, char *local_snd, char *distant_snd)
 {
-  int door;
-  struct char_data *ch;
-  struct room_data *rp, *orp;
+    int             door;
+    struct char_data *ch;
+    struct room_data *rp,
+                   *orp;
 
-  rp = real_roomp(room);
+    rp = real_roomp(room);
 
-  if (rp) {
+    if (!rp) {
+        return;
+    }
+
     for (ch = rp->people; ch; ch = ch->next_in_room) {
-      if (!IS_NPC(ch) && (!IS_AFFECTED(ch, AFF_SILENCE))) {
-         send_to_char(local_snd, ch);
-      }
+        if (!IS_NPC(ch) && !IS_AFFECTED(ch, AFF_SILENCE)) {
+            send_to_char(local_snd, ch);
+        }
     }
+
     for (door = 0; door <= 5; door++) {
-      if (rp->dir_option[door] &&
-	  (orp = real_roomp(rp->dir_option[door]->to_room)) ) {
-	for (ch = orp->people; ch; ch = ch->next_in_room) {
-	  if (!IS_NPC(ch) && (!IS_SET(ch->specials.act, PLR_DEAF)) &&
-	      (!IS_AFFECTED(ch, AFF_SILENCE))) {
-   	 		send_to_char(distant_snd, ch);
-	  }
-	}
-      }
+        if (rp->dir_option[door] &&
+            (orp = real_roomp(rp->dir_option[door]->to_room))) {
+            for (ch = orp->people; ch; ch = ch->next_in_room) {
+                if (!IS_NPC(ch) && !IS_SET(ch->specials.act, PLR_DEAF) && 
+                    !IS_AFFECTED(ch, AFF_SILENCE)) {
+                    send_to_char(distant_snd, ch);
+                }
+            }
+        }
     }
-  }
 }
 
-MakeSound(int pulse)
+void MakeSound(int pulse)
 {
-  int room;
-  char buffer[128];
-  struct obj_data *obj;
-  struct char_data *ch;
+    int             room;
+    char            buffer[128];
+    struct obj_data *obj;
+    struct char_data *ch;
 
-/*
- *  objects
- */
+    /*
+     *  objects
+     */
 
-  for (obj = object_list; obj; obj = obj->next) {
-    if (ITEM_TYPE(obj) == ITEM_AUDIO) {
-      if (((obj->obj_flags.value[0]) &&
-	   (pulse % obj->obj_flags.value[0])==0) ||
-	  (!number(0,5))) {
-	if (obj->carried_by) {
-	  room = obj->carried_by->in_room;
-	} else if (obj->equipped_by) {
-	  room = obj->equipped_by->in_room;
-	} else if (obj->in_room != NOWHERE) {
-	  room = obj->in_room;
-	} else {
-	  room = RecGetObjRoom(obj);
-	}
-	/*
-	 *  broadcast to room
-	 */
+    for (obj = object_list; obj; obj = obj->next) {
+        if (ITEM_TYPE(obj) == ITEM_AUDIO && 
+            ((obj->obj_flags.value[0] && !(pulse % obj->obj_flags.value[0])) ||
+             !number(0, 5))) {
+            if (obj->carried_by) {
+                room = obj->carried_by->in_room;
+            } else if (obj->equipped_by) {
+                room = obj->equipped_by->in_room;
+            } else if (obj->in_room != NOWHERE) {
+                room = obj->in_room;
+            } else {
+                room = RecGetObjRoom(obj);
+            }
 
-	if (obj->action_description) {
-	  MakeNoise(room, obj->action_description, obj->action_description);
-	}
-      }
+            /*
+             *  broadcast to room
+             */
+            if (obj->action_description) {
+                MakeNoise(room, obj->action_description,
+                          obj->action_description);
+            }
+        }
     }
-  }
 
-/*
- *   mobiles
- */
-	for (ch = character_list; ch; ch = ch->next) {
-		if (IS_NPC(ch) && (ch->player.sounds) && (number(0,5)==0)) {
-			if(strcmp(ch->player.sounds,"")) { /* don't make sound if empty sound string */
-				if (ch->specials.default_pos > POSITION_SLEEPING) {
-					if (GET_POS(ch) > POSITION_SLEEPING) {
-						/* Make the sound */
-//						if(*ch->player.distant_snds != '\'')
-							MakeNoise(ch->in_room, ch->player.sounds, ch->player.distant_snds);
-//						else
-//							MakeNoise(ch->in_room, ch->player.sounds,"");
-					} else if (GET_POS(ch) == POSITION_SLEEPING) {
-						/* snore */
-						sprintf(buffer, "%s snores loudly.\n\r", ch->player.short_descr);
-						MakeNoise(ch->in_room, buffer, "You hear a loud snore nearby.\n\r");
-					}
-				} else if (GET_POS(ch) == ch->specials.default_pos) {
-					/* Make the sound */
-//					if(*ch->player.distant_snds != '\'')
-						MakeNoise(ch->in_room, ch->player.sounds, ch->player.distant_snds);
-//					else
-//						MakeNoise(ch->in_room,ch->player.sounds,"");
-				}
-			}
-		}
-	}
+    /*
+     *   mobiles
+     */
+    for (ch = character_list; ch; ch = ch->next) {
+        if (IS_NPC(ch) && (ch->player.sounds) && !number(0, 5) && 
+            strcmp(ch->player.sounds, "")) {
+            /* don't make sound if empty sound string */
+            if (ch->specials.default_pos > POSITION_SLEEPING) {
+                if (GET_POS(ch) > POSITION_SLEEPING) {
+                    /*
+                     * Make the sound 
+                     */
+                    // if(*ch->player.distant_snds != '\'')
+                    MakeNoise(ch->in_room, ch->player.sounds,
+                              ch->player.distant_snds);
+                    // else
+                    // MakeNoise(ch->in_room, ch->player.sounds,"");
+                } else if (GET_POS(ch) == POSITION_SLEEPING) {
+                    /*
+                     * snore 
+                     */
+                    sprintf(buffer, "%s snores loudly.\n\r",
+                            ch->player.short_descr);
+                    MakeNoise(ch->in_room, buffer,
+                              "You hear a loud snore nearby.\n\r");
+                }
+            } else if (GET_POS(ch) == ch->specials.default_pos) {
+                /*
+                 * Make the sound 
+                 */
+                // if(*ch->player.distant_snds != '\'')
+                MakeNoise(ch->in_room, ch->player.sounds,
+                          ch->player.distant_snds);
+                // else
+                // MakeNoise(ch->in_room,ch->player.sounds,"");
+            }
+        }
+    }
 }
 
-
+/*
+ * vim:ts=4:sw=4:ai:et:si:sts=4
+ */
