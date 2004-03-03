7c5f51d258afcc8039b2589b7ea0a2d355bf9743
gjhurlbu <gjhurlbu@69fa473d-82f3-0310-a870-f1ebe01cfa2b>
2004-03-03 03:04:57 +0000

    - reformatting with 'indent -orig -sob -npsl'
    git-svn-id: https://trac.beirdo.ca:4443/svn/havokmud/trunk@753 69fa473d-82f3-0310-a870-f1ebe01cfa2b

--- havokmud/signals.c
+++ havokmud/signals.c
@@ -5,91 +5,101 @@
 
 #include "protos.h"
 
-void checkpointing(int signal);
-void shutdown_request(int signal);
-void logsig(int signal);
-void hupsig(int signal);
+void            checkpointing(int signal);
+void            shutdown_request(int signal);
+void            logsig(int signal);
+void            hupsig(int signal);
 
-int raw_force_all( char *to_force);
+int             raw_force_all(char *to_force);
 
-void signal_setup()
+void signal_setup(void)
 {
- /*       struct itimerval itime;
-	struct timeval interval;     */
+    /*
+     * struct itimerval itime; struct timeval interval; 
+     */
+    signal(SIGUSR2, shutdown_request);
 
-	signal(SIGUSR2, shutdown_request);
+    /*
+     * just to be on the safe side: 
+     */
+    signal(SIGHUP, hupsig);
+    signal(SIGPIPE, SIG_IGN);
+    signal(SIGINT, hupsig);
+    signal(SIGALRM, logsig);
+    signal(SIGTERM, hupsig);
 
-	/* just to be on the safe side: */
+    /*
+     * set up the deadlock-protection 
+     */
 
-	signal(SIGHUP, hupsig);
-	signal(SIGPIPE, SIG_IGN);
-	signal(SIGINT, hupsig);
-	signal(SIGALRM, logsig);
-	signal(SIGTERM, hupsig);
-
-	/* set up the deadlock-protection */
-
-/*        interval.tv_sec = 900;
-	interval.tv_usec = 0;
-	itime.it_interval = interval;
-	itime.it_value = interval;
-      setitimer(ITIMER_VIRTUAL, &itime, 0);
-	signal(SIGVTALRM, checkpointing);  disabled for testing */
+    /*
+     * disabled for testing 
+     *
+     * interval.tv_sec = 900; 
+     * interval.tv_usec = 0; 
+     * itime.it_interval = interval; 
+     * itime.it_value = interval; 
+     * setitimer(ITIMER_VIRTUAL, &itime, 0); 
+     * signal(SIGVTALRM, checkpointing); 
+     */
 }
-
-
 
 void checkpointing(int signal)
 {
-	extern int tics;
-	
-#if 1 
-	if (!tics) {
-		log("CHECKPOINT shutdown: tics not updated");
-	      /*        abort();    temp removal for debug */
-	}
-	else
-	 tics = 0;
+    extern int      tics;
+
+#if 1
+    if (!tics) {
+        log("CHECKPOINT shutdown: tics not updated");
+        /*
+         * abort(); temp removal for debug 
+         */
+    } else
+        tics = 0;
 #else
-if (!tics) {
-	log("CHECKPOINT tics not updated, setting to 1, be very warry");
-	tics=1;
- }
-  else
-     tics = 0;
+    if (!tics) {
+        log("CHECKPOINT tics not updated, setting to 1, be very wary");
+        tics = 1;
+    } else
+        tics = 0;
 #endif
-	 
+
 }
-
-
-
 
 void shutdown_request(int signal)
 {
-	extern int mudshutdown;
+    extern int      mudshutdown;
 
-	log("Received USR2 - shutdown request");
-	mudshutdown = 1;
+    log("Received USR2 - shutdown request");
+    mudshutdown = 1;
 }
 
-
-/* kick out players etc */
+/*
+ * kick out players etc 
+ */
 void hupsig(int signal)
 {
-  int i;
-  extern int mudshutdown, reboot;
+    int             i;
+    extern int      mudshutdown,
+                    reboot;
 
-  log("Received SIGHUP, SIGINT, or SIGTERM. Shutting down");
+    log("Received SIGHUP, SIGINT, or SIGTERM. Shutting down");
 
-  raw_force_all("return");
-  raw_force_all("save");
-  for (i=0;i<30;i++) {
-    SaveTheWorld();
-  }
-  mudshutdown = reboot = 1;
+    raw_force_all("return");
+    raw_force_all("save");
+
+    for (i = 0; i < 30; i++) {
+        SaveTheWorld();
+    }
+
+    mudshutdown = reboot = 1;
 }
 
 void logsig(int signal)
 {
-	log("Signal received. Ignoring.");
+    log("Signal received. Ignoring.");
 }
+
+/*
+ * vim:ts=4:sw=4:ai:et:si:sts=4
+ */
