525ed586c76c80d5b31e8e616a49b49250dfdeb1
gjhurlbu <gjhurlbu@69fa473d-82f3-0310-a870-f1ebe01cfa2b>
2004-03-10 23:26:13 +0000

    - reformatted with 'indent -orig -sob -npsl'
    git-svn-id: https://trac.beirdo.ca:4443/svn/havokmud/trunk@811 69fa473d-82f3-0310-a870-f1ebe01cfa2b

--- havokmud/heap.h
+++ havokmud/heap.h
@@ -1,10 +1,18 @@
+#ifndef _heap_h
+#define _heap_h
 
 struct StrHeapList {
-   char *string;   /* the matching string */
-   int  total;     /* total # of occurences */
+    char           *string;     /* the matching string */
+    int             total;      /* total # of occurences */
 };
 
 struct StrHeap {
-   int uniq;   /* number of uniq items in list */
-   struct StrHeapList *str;   /* the list of strings and totals */
+    int             uniq;       /* number of uniq items in list */
+    struct StrHeapList *str;    /* the list of strings and totals */
 };
+
+#endif
+
+/*
+ * vim:ts=4:sw=4:ai:et:si:sts=4
+ */
