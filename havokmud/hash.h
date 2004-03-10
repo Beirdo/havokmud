9fad6075e9faa8e9a681d2191cea8977f2056242
gjhurlbu <gjhurlbu@69fa473d-82f3-0310-a870-f1ebe01cfa2b>
2004-03-10 22:58:05 +0000

    - reformatted with 'indent -orig -sob -npsl'
    git-svn-id: https://trac.beirdo.ca:4443/svn/havokmud/trunk@810 69fa473d-82f3-0310-a870-f1ebe01cfa2b

--- havokmud/hash.h
+++ havokmud/hash.h
@@ -1,18 +1,28 @@
+#ifndef _hash_h
+#define _hash_h
 
 struct hash_link {
-  int	key;
-  struct hash_link *next;
-  void	*data;
+    int             key;
+    struct hash_link *next;
+    void           *data;
 };
 
 struct hash_header {
-  int	rec_size;
-  int	table_size;
-  int	*keylist, klistsize, klistlen; /* this is really lame,
-					  AMAZINGLY lame */
-  struct hash_link	**buckets;
+    int             rec_size;
+    int             table_size;
+    int            *keylist,
+                    klistsize,
+                    klistlen;   /* this is really lame, AMAZINGLY lame */
+    struct hash_link **buckets;
 };
 
+#define WORLD_SIZE 60000        /* raised due to limit being hit - Mythos
+                                 * 9-25-01 */
+/*
+ * Lowered a bit just because... (from 80k to 60k) 
+ */
+#endif
 
-#define WORLD_SIZE 60000  /* raised due to limit being hit - Mythos 9-25-01 */
-/*Lowered a bit just because... (from 80k to 60k) */
+/*
+ * vim:ts=4:sw=4:ai:et:si:sts=4
+ */
