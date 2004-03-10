d0208561ce81fa58cd35eed0fc647a6cc618c91c
gjhurlbu <gjhurlbu@69fa473d-82f3-0310-a870-f1ebe01cfa2b>
2004-03-10 22:47:45 +0000

    - reformatted with 'indent -orig -sob -npsl'
    git-svn-id: https://trac.beirdo.ca:4443/svn/havokmud/trunk@809 69fa473d-82f3-0310-a870-f1ebe01cfa2b

--- havokmud/handler.h
+++ havokmud/handler.h
@@ -1,3 +1,6 @@
+#ifndef _handler_h
+#define _handler_h
+
 #define FIND_CHAR_ROOM     1
 #define FIND_CHAR_WORLD    2
 #define FIND_OBJ_INV       4
@@ -5,3 +8,8 @@
 #define FIND_OBJ_WORLD    16
 #define FIND_OBJ_EQUIP    32
 
+#endif
+
+/*
+ * vim:ts=4:sw=4:ai:et:si:sts=4
+ */
