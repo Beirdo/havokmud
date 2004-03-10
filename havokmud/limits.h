60a860d0a2ee2e2b6fe31f9453c05cfea9e89e79
gjhurlbu <gjhurlbu@69fa473d-82f3-0310-a870-f1ebe01cfa2b>
2004-03-10 23:37:58 +0000

    - reformatted with 'indent -orig -sob -npsl'
    git-svn-id: https://trac.beirdo.ca:4443/svn/havokmud/trunk@813 69fa473d-82f3-0310-a870-f1ebe01cfa2b

--- havokmud/limits.h
+++ havokmud/limits.h
@@ -1,7 +1,14 @@
+#ifndef _limits_h
+#define _limits_h
 
-struct title_type
-{
-	char *title_m;
-	char *title_f;
-	long exp;
+struct title_type {
+    char           *title_m;
+    char           *title_f;
+    long            exp;
 };
+
+#endif
+
+/*
+ * vim:ts=4:sw=4:ai:et:si:sts=4
+ */
