4305ae32c5ccb299765e6306bebaa4e20b7cf1d2
gjhurlbu <gjhurlbu@69fa473d-82f3-0310-a870-f1ebe01cfa2b>
2004-03-10 23:57:00 +0000

    - reformatted with 'indent -orig -sob -npsl'
    git-svn-id: https://trac.beirdo.ca:4443/svn/havokmud/trunk@816 69fa473d-82f3-0310-a870-f1ebe01cfa2b

--- havokmud/parser.h
+++ havokmud/parser.h
@@ -1,22 +1,29 @@
- 
-#define MAGIC    96  /* Magic number for the hash table */
- 
+#ifndef _parser_h
+#define _parser_h
+
+#define MAGIC    96             /* Magic number for the hash table */
 
 typedef struct command_node NODE;
- 
+
 struct command_node {
-   char *name;
-   void (*func)(struct char_data *ch, char *arg, int cmd);
-   int number;
-   byte min_pos;
-   byte min_level;
-   byte log;
-   struct command_node *next;
-   struct command_node *previous;
+    char           *name;
+    void            (*func) (struct char_data * ch, char *arg, int cmd);
+    int             number;
+    byte            min_pos;
+    byte            min_level;
+    byte            log;
+    struct command_node *next;
+    struct command_node *previous;
 };
- 
+
 struct radix_list {
-   struct command_node *next;
-   unsigned short int number;
-   byte max_len;
+    struct command_node *next;
+    unsigned short int number;
+    byte            max_len;
 };
+
+#endif
+
+/*
+ * vim:ts=4:sw=4:ai:et:si:sts=4
+ */
