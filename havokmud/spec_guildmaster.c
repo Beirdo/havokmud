e38e6139c9567f724aeebe5f903c1aa6013e5b92
gjhurlbu <gjhurlbu@69fa473d-82f3-0310-a870-f1ebe01cfa2b>
2004-05-18 02:45:23 +0000

    - fix bug 952843 - hunter's check should now be correct
    git-svn-id: https://trac.beirdo.ca:4443/svn/havokmud/branches/rev_1_11@1003 69fa473d-82f3-0310-a870-f1ebe01cfa2b

--- havokmud/spec_guildmaster.c
+++ havokmud/spec_guildmaster.c
@@ -200,14 +200,11 @@ int hunter(struct char_data *ch, int cmd, char *arg, struct char_data *mob,
     if (check_soundproof(ch)) {
         return (FALSE);
     }
-    for (; *arg == ' '; arg++) {
-        /* 
-         * ditch spaces 
-         */
-    }
 
-    if ((cmd == 164) || (cmd == 170)) {
-        if (!arg || (strlen(arg) == 0)) {
+    arg = skip_spaces(arg);
+
+    if (cmd == 164 || cmd == 170) {
+        if (!arg || !*arg) {
             sprintf(buf, " hunt           :  %s\n\r",
                     how_good(ch->skills[SKILL_HUNT].learned));
             send_to_char(buf, ch);
@@ -228,41 +225,41 @@ int hunter(struct char_data *ch, int cmd, char *arg, struct char_data *mob,
             send_to_char(buf, ch);
 
             return (TRUE);
-        } else {
-            number = old_search_block(arg, 0, strlen(arg), n_skills, FALSE);
-            send_to_char("The hunter says ", ch);
-            if (number == -1) {
-                send_to_char("'I do not know of this skill.'\n\r", ch);
-                return (TRUE);
-            }
+        } 
 
-            charge = GetMaxLevel(ch) * 100;
-            switch (number) {
-            case 0:
-            case 1:
-                sk_num = SKILL_HUNT;
-                break;
-            case 2:
-                sk_num = SKILL_FIND_TRAP;
-                break;
-            case 3:
-                sk_num = SKILL_REMOVE_TRAP;
-                break;
-            case 4:
-                sk_num = SKILL_EVALUATE;
-                break;
-            case 5:
-                sk_num = SKILL_FIND_FOOD;
-                break;
-            case 6:
-                sk_num = SKILL_FIND_WATER;
-                break;
-            default:
-                sprintf(buf, "Strangeness in hunter (%d)", number);
-                Log(buf);
-                send_to_char("'Ack!  I feel faint!'\n\r", ch);
-                return (FALSE);
-            }
+        number = old_search_block(arg, 0, strlen(arg), n_skills, FALSE);
+        send_to_char("The hunter says ", ch);
+        if (number == -1) {
+            send_to_char("'I do not know of this skill.'\n\r", ch);
+            return (TRUE);
+        }
+
+        charge = GetMaxLevel(ch) * 100;
+        switch (number) {
+        case 0:
+        case 1:
+            sk_num = SKILL_HUNT;
+            break;
+        case 2:
+            sk_num = SKILL_FIND_TRAP;
+            break;
+        case 3:
+            sk_num = SKILL_REMOVE_TRAP;
+            break;
+        case 4:
+            sk_num = SKILL_EVALUATE;
+            break;
+        case 5:
+            sk_num = SKILL_FIND_FOOD;
+            break;
+        case 6:
+            sk_num = SKILL_FIND_WATER;
+            break;
+        default:
+            sprintf(buf, "Strangeness in hunter (%d)", number);
+            Log(buf);
+            send_to_char("'Ack!  I feel faint!'\n\r", ch);
+            return (FALSE);
         }
 
         if (!HasClass(ch, CLASS_THIEF) &&
@@ -273,8 +270,8 @@ int hunter(struct char_data *ch, int cmd, char *arg, struct char_data *mob,
             return (TRUE);
         }
 
-        if ((sk_num == SKILL_HUNT) && (!HasClass(ch, CLASS_THIEF) ||
-                                       !HasClass(ch, CLASS_BARBARIAN))) {
+        if (sk_num == SKILL_HUNT && !HasClass(ch, CLASS_THIEF) && 
+            !HasClass(ch, CLASS_BARBARIAN)) {
             send_to_char("'You're not the sort I'll teach track to.'\n\r", ch);
             return (TRUE);
         }
