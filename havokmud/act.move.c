4a6549a25af1be79a2118d494dfdac4057d5dc79
gjhurlbu <gjhurlbu@69fa473d-82f3-0310-a870-f1ebe01cfa2b>
2004-06-14 06:25:43 +0000

    - fix "follow self"
    git-svn-id: https://trac.beirdo.ca:4443/svn/havokmud/branches/rev_1_11@1115 69fa473d-82f3-0310-a870-f1ebe01cfa2b

--- havokmud/act.move.c
+++ havokmud/act.move.c
@@ -753,7 +753,7 @@ int find_door(struct char_data *ch, char *type, char *dir)
 
     struct room_direction_data *exitp;
 
-    if( !type ) {
+    if(!type) {
         return( -1 );
     }
 
@@ -1767,7 +1767,7 @@ void do_follow(struct char_data *ch, char *argument, int cmd)
     argument = get_argument(argument, &name);
     if (name) {
         if (strcasecmp(name, "self") == 0) {
-            sprintf(name, "%s", GET_NAME(ch));
+            name = GET_NAME(ch);
         }
 
         if (!(leader = get_char_room_vis(ch, name))) {
