9b20408accbea65650ce836dc6c1d2a2c78b61ac
Gavin Hurlbut <gjhurlbu@gmail.com>
2013-10-15 16:58:25 -0700

    Beginnings of DB Upgrade thread

---
+++ src/havokmud/thread/DatabaseUpgradeThread.cpp
@@ -0,0 +1,57 @@
+/*
+ *  This file is part of the havokmud package
+ *  Copyright (C) 2013 Gavin Hurlbut
+ *
+ *  havokmud is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+/**
+ * @file
+ * @brief Thread to handle database upgrades 
+ */
+
+#include <string>
+
+#include "thread/DatabaseUpgradeThread.hpp"
+#include "thread/DatabaseThread.hpp"
+#include "corefunc/Logging.hpp"
+#include "objects/Settings.hpp"
+
+namespace havokmud {
+    namespace thread {
+
+        DatabaseUpgradeThread::DatabaseUpgradeThread(int supportedVersion) :
+                HavokThread("DatabaseUpgrade"),
+                m_supportedVersion(supportedVersion)
+        {
+            pro_initialize<DatabaseUpgradeThread>();
+        }
+
+        void DatabaseThread::start()
+        {
+            int currentVersion = g_settings.get<int>("dbSchema");
+            LogPrint(LG_INFO, "Database schema version %d, supported %d",
+                     currentVersion, m_supportedVersion);
+            while (currentVersion < m_supportedVersion)
+            {
+                // Upgrade from current to current+1
+                currentVersion++;
+                g_setting.set<int>("dbSchema", currentVersion);
+            }
+            m_startupMutex.unlock();
+        }
+    }
+}
+
---
+++ src/havokmud/thread/DatabaseUpgradeThread.hpp
@@ -0,0 +1,60 @@
+/*
+ *  This file is part of the havokmud package
+ *  Copyright (C) 2013 Gavin Hurlbut
+ *
+ *  havokmud is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+/**
+ * @file
+ * @brief Thread to handle database upgrades
+ */
+
+#ifndef __havokmud_thread_DatabaseUpgradeThread__
+#define __havokmud_thread_DatabaseUpgradeThread__
+
+#include <string>
+#include <cppconn/driver.h>
+#include <cppconn/connection.h>
+
+#include "thread/HavokThread.hpp"
+#include "objects/LockingQueue.hpp"
+#include "objects/DatabaseResponse.hpp"
+#include "objects/DatabaseRequest.hpp"
+
+namespace havokmud {
+    namespace thread {
+
+        using havokmud::thread::HavokThread;
+
+        typedef boost::shared_ptr<DatabaseRequest>  RequestPointer;
+        typedef boost::shared_ptr<DatabaseResponse> ResponsePointer;
+
+        class DatabaseUpgradeThread : public HavokThread
+        {
+        public:
+            DatabaseUpgradeThread(int supportedVersion);
+            ~DatabaseUpgradeThread()  {};
+
+            virtual void start();
+            void handle_stop()  {};
+
+        private:
+            int m_supportedVersion;
+        };
+    }
+}
+
+#endif  // __havokmud_thread_DatabaseUpgradeThread__
